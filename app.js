a5276eef59231f4bb3ba574de285f3ae30978196
Fabian Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2018-07-06 10:49:03 -0400

    Con animacion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //funcionalidad del side Menú
 function toggleMenu() { // añadir función onclick="toggleMenu()" al botón del nav bar y al botón cerrar.
-  if (sideMenu.className.indexOf("d-none") >= 0) { // primero revisamos si la clase d-none esta
+  if (sideMenu.className.indexOf("menu_closed") >= 0) { // primero revisamos si la clase d-none esta
     openMenu();  // si esta la clase quiere decir que el menú esta cerrado, asi que llamamos la funcion para abrirlo
   } else {
     closeMenu(); //si no esta la clase, le indicamos que cierre el menu
@@ -8,9 +8,11 @@ function toggleMenu() { // añadir función onclick="toggleMenu()" al botón del
 }
 
 function openMenu() {
-  sideMenu.classList.remove('d-none'); // quitando clase display-none 
+  sideMenu.classList.remove('menu_closed'); // quitando clase display-none
+  sideMenu.classList.add('menu_open');
 }
 
 function closeMenu() {
-  sideMenu.classList.add('d-none'); // añadimos la clase display-none
+  sideMenu.classList.add('menu_closed'); // añadimos la clase display-none
+  sideMenu.classList.remove('menu_open');
 }
